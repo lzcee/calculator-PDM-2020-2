5e37f498ec7b6abe31147d1f173f155d1676d4ff
Letícia Cellurale <leticia.cellurale@gmail.com>
2020-10-20 21:55:41 -0300

    :sparkles: create onClick method

--- app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
+++ app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.calculadorapdm20202;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView displayTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayTv = findViewById(R.id.displayTv);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button)view;
+        String textButton = button.getText().toString();
+
+        if (view.getId() == R.id.clearBt) {
+            displayTv.setText("");
+        } else {
+            displayTv.setText(displayTv.getText().toString() + textButton);
+        }
     }
 }
